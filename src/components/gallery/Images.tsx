d6b1898f8fdbd8147623eb59412621efd52f35ac
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2022-09-28 20:20:54 +0400

    added zoom effetcs to gallery

--- src/components/gallery/Images.tsx
+++ src/components/gallery/Images.tsx
@@ -9,16 +9,18 @@ export type imagesToShow = {
 
 const Images = ({ imagesToShow }: ImagesProp) => {
   return (
-    <section className="image-list grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-1 mx-auto justify-items-center overflow-hidden w-full ">
+    <section className="image-list  grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-1 mx-auto justify-items-center overflow-hidden w-full ">
       {imagesToShow.map((img, index) => (
-        <img
-          key={index}
-          src={img.srcMobile}
-          srcSet={`${img.srcMobile} 300w, ${img.src} 900w`}
-          sizes="(min-width: 660px) 300px, 100vw"
-          alt=""
-          className=" h-full w-full block object-cover cursor-pointer hover:scale-125 hover:border-2 smooth-transition"
-        ></img>
+        <section className="card-zoom">
+          <img
+            key={index}
+            src={img.srcMobile}
+            srcSet={`${img.srcMobile} 300w, ${img.src} 900w`}
+            sizes="(min-width: 660px) 300px, 100vw"
+            alt=""
+            className="card-zoom-image"
+          ></img>
+        </section>
       ))}
     </section>
   );
