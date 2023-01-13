6cc1acb76fb200999704ba3f17d9702eea8b9d89
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-01-13 09:40:24 -0300

    fixed blog sizes

--- src/components/blog/BlogPosts.tsx
+++ src/components/blog/BlogPosts.tsx
@@ -1,22 +1,21 @@
-import BlogPostsData from "../../data/BlogPostsData";
-
+import BlogPostsData from '../../data/BlogPostsData'
 
 const BlogPosts = () => {
   return (
-    <section className="grid sm:grid-cols-1 md:grid-cols-2  gap-1 mx-auto  justify-items-stretch overflow-hidden  w-10/12">
+    <section className="grid sm:grid-cols-1 md:grid-cols-2  gap-2 mx-auto  justify-items-stretch overflow-hidden  w-10/12 mt-20">
       {BlogPostsData.reverse().map((post) => (
-        <section className="grid grid-cols-1 gap-1 mt-20" key={post.id}>
+        <section className="grid grid-cols-1 gap-2 h-1/5" key={post.id}>
           <img
             src={post.src}
             alt={post.alt}
-            className="w-full h-80 block object-cover cursor-pointer smooth-transition hover:opacity-80 smooth-transition"
+            className="w-full h-96 block object-cover cursor-pointer smooth-transition hover:opacity-80 smooth-transition"
           ></img>
           <h2 className="text-2xl uppercase text-black">{post.title}</h2>
           <p className="text-md">{post.snippet}</p>
         </section>
       ))}
     </section>
-  );
-};
+  )
+}
 
-export default BlogPosts;
+export default BlogPosts
--- src/components/gallery/Images.tsx
+++ src/components/gallery/Images.tsx
@@ -10,9 +10,9 @@ export type imagesToShow = {
 
 const Images = ({ imagesToShow }: ImagesProp) => {
   return (
-    <section className="image-list  grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-1 mx-auto justify-items-center overflow-hidden w-full ">
-      {imagesToShow.map((img) => (
-        <section className="card-zoom" key={img.id}>
+    <section className="image-list  grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-1 mx-auto overflow-hidden w-full ">
+      {imagesToShow.map((img, index) => (
+        <section className="card-zoom" key={index}>
           <img src={img.src} alt={img.alt} className="card-zoom-image"></img>
         </section>
       ))}
