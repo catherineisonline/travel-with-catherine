03f882314c767e393c779ce24d56bda027e9813b
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-08-03 19:52:19 +0400

    updated slider

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import  { useState, useEffect } from 'react'
 import GalleryData from '../../data/GalleryData.js'
 import ReactPaginate from 'react-paginate';
 import Images from './Images'
@@ -18,11 +18,14 @@ export interface ImagesProp {
   setModalImg: (imgIndex: number) => void;
 
 }
+
 const Gallery = () => {
   const [itemOffset, setItemOffset] = useState(0);
   const [endOffset, setEndOffset] = useState(itemOffset + 12);
   const [currentBlogPosts, setcurrentBlogPosts] = useState([...GalleryData].slice(itemOffset, endOffset));
   const [isOpen, setisOpen] = useState(false);
+  const [targetImg, setTargetImg] = useState(0);
+  const [targetBtn, setTargetBtn] = useState("")
 
 
   const handlePageClick = (event: any) => {
@@ -34,8 +37,8 @@ const Gallery = () => {
     setisOpen(!isOpen);
   };
 
-  const setModalImg = (imgIndex: number)=>  {
-    console.log(imgIndex);
+  const setModalImg = (imgIndex: number) => {
+    setTargetImg(imgIndex);
   }
 
   useEffect(() => {
@@ -44,9 +47,36 @@ const Gallery = () => {
     setcurrentBlogPosts([...GalleryData].slice(itemOffset, endOffset));
   }, [setEndOffset, endOffset, itemOffset]);
 
+
+  useEffect(() => {
+    // ...
+
+    const handleNextClick = () => {
+      setTargetBtn("next");
+      setTargetImg((prevImg) => (prevImg + 1) % currentBlogPosts.length);
+    };
+
+    const handlePrevClick = () => {
+      setTargetBtn("prev");
+      setTargetImg((prevImg) => (prevImg - 1 + currentBlogPosts.length) % currentBlogPosts.length);
+    };
+
+    window.onclick = (event: MouseEvent) => {
+      const target = event.target as Element;
+      const nextBtn = target.getAttribute("data-type") === "next";
+      const prevBtn = target.getAttribute("data-type") === "prev";
+      
+      if (nextBtn) {
+        handleNextClick();
+      }
+      if (prevBtn) {
+        handlePrevClick();
+      }
+    };
+  }, [currentBlogPosts.length]);
   return (
     <article className="sm:col-span-5 md:col-span-4 min-h-screen scrollbar-hide">
-      <Images imagesToShow={currentBlogPosts} toggle={toggle} setModalImg={setModalImg}/>
+      <Images imagesToShow={currentBlogPosts} toggle={toggle} setModalImg={setModalImg} />
       <section className="flex flex-col items-center w-full">
         <ReactPaginate
           className="pagination"
@@ -59,14 +89,18 @@ const Gallery = () => {
         />
       </section>
       {isOpen ?
-      //Modal
-       <section className="img-modal w-screen h-screen fixed z-30 top-0 left-0 flex flex-col content-center items-center bg-rgba(255, 255, 255, .15) backdrop-blur-sm">
+        //Modal
+        <section className="img-modal w-screen h-screen fixed z-30 top-0 left-0 flex flex-col content-center items-center bg-rgba(255, 255, 255, .15) backdrop-blur-sm">
           <button onClick={toggle} className="absolute text-2xl text-gray-200 text-bold p-2 bg-black rounded-full	w-10 h-10 right-4 top-4 flex items-center justify-center hover:-rotate-90 transition-all duration-500	">X</button>
-        <Slide>
-          {GalleryData.map((img, index) => (
-            <img key={index} src={img.src} alt='' className='w-full object-cover h-72' />
-          ))}
-        </Slide>
+          <Slide
+          transitionDuration={400}
+          >
+            <img   key={currentBlogPosts[targetImg].id}
+          src={targetBtn === "" ? currentBlogPosts[targetImg].src : currentBlogPosts[targetImg].src}
+          alt={currentBlogPosts[targetImg].alt}
+          className='w-full object-cover h-72' />
+          </Slide>
+
         </section> :
         null}
     </article>
--- src/components/gallery/Images.tsx
+++ src/components/gallery/Images.tsx
@@ -1,12 +1,12 @@
 import {ImagesProp } from './Gallery';
 
 
-const Images = ({ imagesToShow, toggle }: ImagesProp) => {
+const Images = ({ imagesToShow, toggle, setModalImg }: ImagesProp) => {
   return (
     <section className="image-list grid sm:grid-cols-1 md:grid-cols-2 2xl:grid-cols-3  gap-1 mx-auto overflow-hidden w-full ">
       {imagesToShow.map((img, index) => (
         <section className="card-zoom" key={index}>
-          <img onClick={() => toggle(index)} src={img.src} alt={img.alt} className="card-zoom-image" />
+          <img onClick={() => {toggle(); setModalImg(index)}} src={img.src} alt={img.alt} className="card-zoom-image" />
         </section>
       ))}
     </section>
