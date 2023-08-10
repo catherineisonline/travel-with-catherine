5477bec8e1d0a46c571acb0b183dd3687213fb52
Ekaterine (Catherine) Mitagvaria <63928803+catherineisonline@users.noreply.github.com>
2023-08-10 15:15:08 +0400

    updated gallery modal

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,9 +1,8 @@
-import  { useState, useEffect } from 'react'
+import  { useState, useEffect, useCallback } from 'react'
 import GalleryData from '../../data/GalleryData';
 import ReactPaginate from 'react-paginate';
 import Images from './Images'
 import ResetLocation from '../../helpers/ResetLocation';
-import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css'
 
 export type imagesToShow = {
@@ -20,12 +19,20 @@ export interface ImagesProp {
 }
 
 const Gallery = () => {
+  
   const [itemOffset, setItemOffset] = useState(0);
   const [endOffset, setEndOffset] = useState(itemOffset + 12);
   const [currentBlogPosts, setcurrentBlogPosts] = useState([...GalleryData].slice(itemOffset, endOffset));
   const [isOpen, setisOpen] = useState(false);
   const [targetImg, setTargetImg] = useState(0);
-  const [targetBtn, setTargetBtn] = useState("")
+
+  const handleNextClick = useCallback(() => {
+    setTargetImg((prevImg) => (prevImg + 1) % currentBlogPosts.length);
+  }, [currentBlogPosts.length]);
+
+  const handlePrevClick = useCallback(() => {
+    setTargetImg((prevImg) => (prevImg - 1 + currentBlogPosts.length) % currentBlogPosts.length);
+  }, [currentBlogPosts.length]);
 
 
   const handlePageClick = (event: any) => {
@@ -48,32 +55,28 @@ const Gallery = () => {
   }, [setEndOffset, endOffset, itemOffset]);
 
 
-  useEffect(() => {
-    // ...
-
-    const handleNextClick = () => {
-      setTargetBtn("next");
-      setTargetImg((prevImg) => (prevImg + 1) % currentBlogPosts.length);
-    };
-
-    const handlePrevClick = () => {
-      setTargetBtn("prev");
-      setTargetImg((prevImg) => (prevImg - 1 + currentBlogPosts.length) % currentBlogPosts.length);
-    };
-
-    window.onclick = (event: MouseEvent) => {
-      const target = event.target as Element;
-      const nextBtn = target.getAttribute("data-type") === "next";
-      const prevBtn = target.getAttribute("data-type") === "prev";
-      
-      if (nextBtn) {
-        handleNextClick();
-      }
-      if (prevBtn) {
-        handlePrevClick();
-      }
-    };
-  }, [currentBlogPosts.length]);
+    useEffect(() => {
+      const handleClick = (event: MouseEvent) => {
+        const target = event.target as Element;
+        const nextBtn = target.getAttribute('data-type') === 'next';
+        const prevBtn = target.getAttribute('data-type') === 'prev';
+    
+        if (nextBtn) {
+          handleNextClick();
+        }
+        if (prevBtn) {
+          handlePrevClick();
+        }
+      };
+    
+      window.addEventListener('click', handleClick);
+    
+      return () => {
+        // Clean up the event listener when the component unmounts
+        window.removeEventListener('click', handleClick);
+      };
+    }, [handleNextClick, handlePrevClick]);
+    
   return (
     <article className="sm:col-span-5 md:col-span-4 min-h-screen scrollbar-hide">
       <Images imagesToShow={currentBlogPosts} toggle={toggle} setModalImg={setModalImg} />
@@ -90,18 +93,36 @@ const Gallery = () => {
       </section>
       {isOpen ?
         //Modal
-        <section className="img-modal w-screen h-screen fixed z-30 top-0 left-0 flex flex-col content-center items-center bg-rgba(255, 255, 255, .15) backdrop-blur-sm">
-          <button onClick={toggle} className="absolute text-2xl text-gray-200 text-bold p-2 bg-black rounded-full	w-10 h-10 right-4 top-4 flex items-center justify-center hover:-rotate-90 transition-all duration-500	">X</button>
-          <Slide
-          transitionDuration={400}
-          >
-            <img   key={currentBlogPosts[targetImg].id}
-          src={targetBtn === "" ? currentBlogPosts[targetImg].src : currentBlogPosts[targetImg].src}
-          alt={currentBlogPosts[targetImg].alt}
-          className='w-full object-cover h-72' />
-          </Slide>
-
-        </section> :
+        <section className={`img-modal w-screen h-screen fixed z-30 top-0 left-0 flex  justify-center bg-rgba(255, 255, 255, .15) backdrop-blur-sm ${isOpen ? 'flex' : 'hidden'}`}>
+        <button onClick={toggle} className="absolute flex items-center justify-center z-40 text-2xl text-gray-200 text-bold p-2 bg-black rounded-full w-10 h-10 right-4 top-4 hover:-rotate-90 transition duration-500">
+          X
+        </button>
+        <button
+          onClick={handlePrevClick}
+          className="absolute flex items-center justify-center z-40 left-8 top-1/2 text-xl text-gray-200 text-bold p-6 bg-black rounded-full w-10 h-10 hover:bg-opacity-75 transition"
+          data-type="prev"
+        >
+          &lt;
+        </button>
+        <section className="slider-container relative w-4/5 flex items-center">
+          {currentBlogPosts.map((image, index) => (
+            <img
+              key={image.id}
+              className={`slider-slide ${targetImg === index ? 'active' : 'hidden'} object-cover object-center w-full h-3/4`}
+              src={image.src}
+              alt=''
+            />
+          ))}
+        </section>
+        <button
+          onClick={handleNextClick}
+          className="absolute z-40 flex items-center justify-center right-8 top-1/2 text-xl text-gray-200 text-bold p-6 bg-black rounded-full w-10 h-10 hover:bg-opacity-75 transition"
+          data-type="next"
+        >
+          &gt;
+        </button>
+      </section>
+       :
         null}
     </article>
   )
